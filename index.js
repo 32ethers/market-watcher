db69fdfdd919565dcba01066129ed9dedfd8186c
32ethers <somereasons@outlook.com>
2023-07-04 15:43:52 +0800

    add ip watcher

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const config = require('./config')
 const app = express()
 const path = require("path")
 
+
 app.set('views', path.join(__dirname, 'views/'));
 app.engine('.html', require('ejs').renderFile);
 app.set('view engine', 'html');
@@ -19,21 +20,25 @@ async function getTokenPrice() {
     for (let i = 0; i < tokens.length; i++) {
         let close = tokenResponse[i].data.data[0].close;
         let open = tokenResponse[i].data.data[0].open;
-        tokenResult.push({ name: tokens[i].name, value: close, rate: (100 * ((close - open) / open)).toFixed(2) });
+        tokenResult.push({name: tokens[i].name, value: close, rate: (100 * ((close - open) / open)).toFixed(2)});
     }
     return tokenResult;
 }
+
 async function getDollarPrice() {
     let resp = await axios.get("https://api.exchangerate-api.com/v4/latest/USD");
     let result = [];
-    result.push({ name: "美元对人民币", value: resp.data.rates.CNY });
+    result.push({name: "美元对人民币", value: resp.data.rates.CNY});
     return result;
 }
+
 async function getStockPrice() {
     const stocks2Query = config.stock;
 
     let queryString = [];
-    stocks2Query.forEach(a => { queryString.push("s_" + a.symbol) })
+    stocks2Query.forEach(a => {
+        queryString.push("s_" + a.symbol)
+    })
     let reqString = `http://qt.gtimg.cn/r=0.8409869808238q=${queryString.join(",")}`;
     // v_s_sz000559="51~万向钱潮~000559~5.81~0.07~1.22~110475~6384~~191.95~GP-A";
     let resp = await axios.get(reqString);
@@ -43,11 +48,12 @@ async function getStockPrice() {
         if (i >= stocks2Query.length)
             continue
         let info = stocks[i].split("~");
-        result.push({ name: stocks2Query[i].name, value: info[3], rate: info[5] })
+        result.push({name: stocks2Query[i].name, value: info[3], rate: info[5]})
     }
 
     return result;
 }
+
 app.get('/', async (req, res) => {
     let result = []
     let [token, dollar, stock] = await Promise.all([getTokenPrice(), getDollarPrice(), getStockPrice()])
@@ -55,11 +61,21 @@ app.get('/', async (req, res) => {
     result.push(...dollar)
     result.push(...stock)
 
-    res.render("index", { items: result })
+    res.render("index", {items: result})
+})
+let ip_str = "unknown"
+
+app.get("/ip", async (req, res) => {
+    res.render("ip", {ip_str: ip_str})
 })
 
+app.get("/report_ip", async (req, res) => {
+    params = req.query
+    ip_str = params["ip"]
+    res.send({"status": "0", "param_echo": ip_str})
+})
 app.listen(config.port, "0.0.0.0", () => {
-    console.log(`Example app listening on port ${config.port}`)
+    console.log(`market watcher app listening on port ${config.port}`)
 })
 
 //http://qt.gtimg.cn/r=0.8409869808238q=s_sz000559,s_sh600895,s_sz002048,s_sz002085,s_sz002126,s_sz002284,s_sz002434,s_sz002472,s_sz002488
