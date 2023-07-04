af525795662a631106e27d4ee8050eda1ae926f7
32ethers <somereasons@outlook.com>
2023-07-04 15:25:08 +0800

    init

---
+++ config-sample.js
@@ -0,0 +1,14 @@
+exports.token = [
+    { name: "BTC", symbol: "btcusdt" },
+    { name: "ETH", symbol: "ethusdt" },
+    { name: "ADA", symbol: "adausdt" }
+]
+
+exports.stock = [
+    { name: "中信证券", symbol: "sh600030" },
+    { name: "芯片ETF", symbol: "sz159995" },
+    { name: "三聚环保", symbol: "sz300072" },
+    { name: "黄金ETF", symbol: "sh518880"}
+]
+
+exports.port = 8088
